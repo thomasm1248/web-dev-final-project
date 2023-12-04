cd4c85f29890108e93b25df67689989c76675721
Alevarias <alexjusticesierra@gmail.com>
2023-12-04 13:42:28 -0700

    rearanging files

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    var navList = document.getElementById("nav-list");
-    if (navList.style.display === "block") {
-        navList.style.display = "none";
-    } else {
-        navList.style.display = "block";
-    }
-}
---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var navList = document.getElementById("nav-list");
+    if (navList.style.display === "block") {
+        navList.style.display = "none";
+    } else {
+        navList.style.display = "block";
+    }
+}
